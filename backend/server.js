7f422d94573b183d7de296f89bade3ca85b00ea4
naman <namanshukla256@gmail.com>
2025-01-01 13:58:54 +0530

    1/1

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000; // process.env.PORT is for deployment
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
     console.log("Server is running on http://localhost:" + PORT);  
