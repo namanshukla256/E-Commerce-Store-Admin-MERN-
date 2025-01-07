6f87e1a69106dd327214288bf2fb2f93fd006053
naman <namanshukla256@gmail.com>
2025-01-07 15:09:50 +0530

    7/1

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -55,25 +55,105 @@ export const signup =  async (req, res) => {
     setCookies (res, accessToken, refreshToken);
 
     res.status(201).json({
-        user: {
             _id: user._id,
             name: user.name,
             email: user.email,
             role: user.role
-        }, message: "User created successfully"
-    });
+       });
 
     } catch (error) {
-        res.status(500).json({
-            message: error.message
-        });
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: error.message });
+    }
+};
+ 
+export const login =  async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if(user && (await user.comparePassword(password))) {
+            const {accessToken, refreshToken } = generateTokens(user.id)
+
+            await storeRefreshToken(user._id, refreshToken)
+            setCookies(res, accessToken, refreshToken)
+
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            });
+        } else {
+            res.status(401).json({ message: "Invalid email or password "})
+        }
+        
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ message: error.message });
     }
 };
 
-export const login =  async (req, res) => {
-    res.send("login route called");
+export const logout =  async (req, res) => {
+    try {
+        const refreshToken  = req.cookies.refreshToken;
+        if(refreshToken){
+            const decode = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+            await redis.del(`refresh_token:${decode.userId}`);
+        }
+
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+        res.json({ message: "Logged out successfully "});
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });   
+    }
+}; 
+
+// this will refresh the access token
+
+  export const refreshToken = async (req, res) => {
+    try { 
+        const refreshToken = req.cookies.refreshToken;
+
+        if(!refreshToken) {
+            return res.status(401).json({ message: "No refresh token provided " });
+        }
+
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const storedToken = await redis.get(`refresh_token:${decoded.userId}`);
+
+        if(storedToken !== refreshToken) {
+            return res.status(401).json({ message: "Invalid refresh token " });
+        }
+
+        const accessToken = jwt.sign({ userId: decoded.userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m"}); 
+
+        res.cookie ("accessToken", accessToken, {
+            httpOnly: true, // prevent XSS attacks, cross-site scripting)
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict", // prevents CSRF attacks, cross-site request forgery
+            maxAge: 15 * 60 * 1000 // 15 minutes
+        });
+
+        res.json({ message: "Token refreshed successfully "});
+     } catch (error) {
+        console.log("Error in refreshToken controller", error.message);
+        res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
-export const logout =  async (req, res) => {
-    res.send("logout route called");
-}; 
+
+/* TO DO => Implement Get profile
+// export const getProfile = async (req, res) => {
+//     try {
+//         const user = await User.findById(req.user.id).select("-password");
+//         res.json(user);
+//     } catch (error) {
+//         console.log("Error in getProfile controller", error.message);
+//         res.status(500).json({ message: "Server error", error: error.message });
+//     }
+// }
+
+*/
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000; // process.env.PORT is for deployment
 
 app.use(express.json()); // allows you to parse incoming JSON data
+app.use(cookieParser()); 
 
 app.use("/api/auth", authRoutes);
 
