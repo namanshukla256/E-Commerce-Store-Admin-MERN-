1dd0991671124771e451cb1d68b046c16e30a856
naman <namanshukla256@gmail.com>
2025-01-08 23:44:05 +0530

    8/1

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -15,6 +16,8 @@ app.use(express.json()); // allows you to parse incoming JSON data
 app.use(cookieParser()); 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+
 
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:" + PORT);  
