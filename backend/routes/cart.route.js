8b0e5f41bc2a823db917403c01399227eb9f4543
naman <namanshukla256@gmail.com>
2025-01-10 21:10:25 +0530

    10/1

---
+++ backend/routes/cart.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { addToCart } from "../controllers/cart.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.post("/", protectRoute, getCartProducts);
+router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
+
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+
 
 import { connectDB } from "./lib/db.js";
 
@@ -17,6 +19,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+
 
 
 app.listen(PORT, () => {
