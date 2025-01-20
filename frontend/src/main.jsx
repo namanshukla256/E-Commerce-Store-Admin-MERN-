a897645a1fe181696399f8f4fd567cd1b13d4c50
naman <namanshukla256@gmail.com>
2025-01-20 18:46:11 +0530

    20

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'; 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
